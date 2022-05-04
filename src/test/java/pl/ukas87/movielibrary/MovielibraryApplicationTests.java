98d5582e81ccf774d431b8205c1fb4f46656bc45
Łukasz Lewandowski <lukastonie@gmail.com>
2022-05-04 21:39:15 +0200

    first commit

---
+++ src/test/java/pl/ukas87/movielibrary/MovielibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ukas87.movielibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovielibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
