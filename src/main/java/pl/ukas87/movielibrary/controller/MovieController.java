9d5713f0e0afd4138abf2f0b85babdf95d9b4f6e
Łukasz Lewandowski <lukastonie@gmail.com>
2022-05-05 13:34:35 +0200

    adding delete, update

--- src/main/java/pl/ukas87/movielibrary/controller/MovieController.java
+++ src/main/java/pl/ukas87/movielibrary/controller/MovieController.java
@@ -1,32 +1,67 @@
 package pl.ukas87.movielibrary.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.ukas87.movielibrary.model.Movie;
 import pl.ukas87.movielibrary.repository.MovieRepository;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/movies")
 public class MovieController {
 
     @Autowired
     MovieRepository movieRepository;
 
-    @GetMapping("/test")
-    public int test() {
-        return 1;
-    }
-
-    @GetMapping("/movies")
+    @GetMapping("")
     public List<Movie> getAll(){
        return movieRepository.getAll();
     }
 
-    @GetMapping("/movies/{id}")
+    @GetMapping("/{id}")
     public Movie getById(@PathVariable("id") int id){
         return movieRepository.getById(id);
+    }
+
+    @PostMapping("")
+    public int add(@RequestBody List<Movie> movies){
+       return movieRepository.save(movies);
+    }
+
+    @PutMapping("/{id}")
+    public int update(@PathVariable("id") int id, @RequestBody Movie updatedMovie){
+        Movie movie = movieRepository.getById(id);
+
+        if(movie != null){
+            movie.setName(updatedMovie.getName());
+            movie.setRating(updatedMovie.getRating());
+            movieRepository.update(movie);
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    @PatchMapping("/{id}")
+    public int partiallyUpdate(@PathVariable("id") int id, @RequestBody Movie updateMovie){
+        Movie movie = movieRepository.getById(id);
+
+        if(movie != null){
+            if (updateMovie.getName() != null) movie.setName(updateMovie.getName());
+            if (updateMovie.getRating() > 0) movie.setRating(updateMovie.getRating());
+
+            movieRepository.update(movie);
+            return 1;
+
+        } else {
+            return -1;
+        }
+
+    }
+
+    @DeleteMapping("/{id}")
+    public int delete(@PathVariable("id") int id){
+        return movieRepository.delete(id);
     }
 }
